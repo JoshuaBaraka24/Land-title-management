5da8a11a0028a1ab182c9708a455015923f6161f
JoshuaBaraka24 <joshua.adalo@strathmore.edu>
2025-06-25 23:56:32 +0300

    Setup 5

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,67 +1,86 @@
-// client/craco.config.js
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      // Add process/browser alias
-      webpackConfig.resolve.alias = {
-        ...webpackConfig.resolve.alias,
-        'process/browser': path.resolve(__dirname, 'node_modules/process/browser.js')
-      };
-
-      module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback,
-        "vm": require.resolve("vm-browserify")
-      };
-      return webpackConfig;
-    }
-  }
-};
-
-      module.exports = {
-  webpack: {
-    configure: {
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            enforce: "pre",
-            use: ["source-map-loader"],
-            exclude: /node_modules\/@truffle\/contract/
-          }
-        ]
+    configure: (webpackConfig, { env, paths }) => {
+      // 1. Remove deprecated middleware options at the very beginning
+      if (webpackConfig.devServer) {
+        delete webpackConfig.devServer.onBeforeSetupMiddleware;
+        delete webpackConfig.devServer.onAfterSetupMiddleware;
+        
+        // Use the new setupMiddlewares API
+        webpackConfig.devServer.setupMiddlewares = (middlewares) => {
+          return middlewares;
+        };
       }
-    }
-  }
-};
 
-      // Add fallbacks
+      // 2. Resolve aliases
+      webpackConfig.resolve.alias = {
+        ...(webpackConfig.resolve.alias || {}),
+        'process/browser': path.resolve(__dirname, 'node_modules/process/browser.js'),
+        'stream': path.resolve(__dirname, 'node_modules/stream-browserify')
+      };
+
+      // 3. Add fallbacks for Node.js core modules
       webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback,
+        ...(webpackConfig.resolve.fallback || {}),
         "crypto": require.resolve("crypto-browserify"),
         "stream": require.resolve("stream-browserify"),
         "os": require.resolve("os-browserify/browser"),
         "path": require.resolve("path-browserify"),
         "url": require.resolve("url/"),
         "assert": require.resolve("assert/"),
-        "process": require.resolve("process/browser")
+        "process": require.resolve("process/browser"),
+        "vm": require.resolve("vm-browserify"),
+        "events": require.resolve("events/"),
+        "buffer": require.resolve("buffer/"),
+        "fs": false
       };
 
-      // Add plugins
+      // 4. Add plugins for global polyfills
       webpackConfig.plugins = [
-        ...webpackConfig.plugins,
+        ...(webpackConfig.plugins || []),
         new webpack.ProvidePlugin({
           process: 'process/browser',
-          Buffer: ['buffer', 'Buffer']
+          Buffer: ['buffer', 'Buffer'],
+          Stream: ['stream-browserify', 'Stream'] 
         })
       ];
 
+      // 5. Handle source map warnings
+      webpackConfig.ignoreWarnings = [
+        ...(webpackConfig.ignoreWarnings || []),
+        { module: /node_modules\/@truffle\// },
+        { module: /node_modules\/web3-eth-contract\// },
+        /Failed to parse source map/,
+        /Critical dependency/
+      ];
+
+      // 6. Add source-map-loader rule with exclusions
+      const sourceMapLoaderRule = {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+        exclude: [
+          /node_modules\/@truffle\//,
+          /node_modules\/web3-eth-contract\//,
+          /node_modules\/nofilter\//,
+          /node_modules\/cbor\//
+        ]
+      };
+      
+      // Add to existing rules without overwriting
+      webpackConfig.module.rules.push(sourceMapLoaderRule);
+
       return webpackConfig;
+    }
+  },
+  eslint: {
+    enable: true,
+    mode: "extends",
+    configure: {
+      useEslintrc: true
     }
   }
 };
